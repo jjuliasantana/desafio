917bd04e53a68d9383dcc00dbfcb64d9718ac991
jjuliasantana <jjuliasantana1@gmail.com>
2025-08-08 23:59:21 -0300

    agora foi

--- index.js
+++ index.js
@@ -3,29 +3,31 @@ let xpHeroi = 15000;
 
 switch (true) {
   case xpHeroi <= 1000:
-    console.log(`O heroi de nome ${nomeHeroi} está no nível de Ferro`);
+    console.log("O heroi de nome " + nomeHeroi + " está no nível de Ferro");
     break;
   case xpHeroi <= 2000:
-    console.log(`O heroi de nome ${nomeHeroi} está no nível de Bronze`);
+    console.log("O heroi de nome " + nomeHeroi + " está no nível de Bronze");
     break;
   case xpHeroi <= 5000:
-    console.log(`O heroi de nome ${nomeHeroi} está no nível de Prata`);
+    console.log("O heroi de nome " + nomeHeroi + " está no nível de Prata");
     break;
   case xpHeroi <= 6000:
-    console.log(`O heroi de nome ${nomeHeroi} está no nível de Ouro`);
+    console.log("O heroi de nome " + nomeHeroi + " está no nível de Ouro");
     break;
   case xpHeroi <= 7000:
-    console.log(`O heroi de nome ${nomeHeroi} está no nível de Platina`);
+    console.log("O heroi de nome " + nomeHeroi + " está no nível dePlatina");
     break;
   case xpHeroi <= 8000:
-    console.log(`O heroi de nome ${nomeHeroi} está no nível de Ascendente`);
+    console.log(
+      "O heroi de nome " + nomeHeroi + " está no nível de Ascendente"
+    );
     break;
   case xpHeroi <= 9000:
-    console.log(`O heroi de nome ${nomeHeroi} está no nível de Imortal`);
+    console.log("O heroi de nome " + nomeHeroi + " está no nível de Imortal");
     break;
   case xpHeroi <= 10000:
-    console.log(`O heroi de nome ${nomeHeroi} está no nível de Radiante`);
+    console.log("O heroi de nome " + nomeHeroi + " está no nível de Radiante");
     break;
   default:
-    console.log(`O heroi de nome ${nomeHeroi} está no nível de Radiante`);
+    console.log("O heroi de nome " + nomeHeroi + " está no nível de Radiante");
 }
